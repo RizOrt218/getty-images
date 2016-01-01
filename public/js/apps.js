aac6fbb4e7a7d77b5acf76e85ee105ff431caccb
RizOrt218 <rizhiafalen@gmail.com>
2015-12-31 14:04:32 -1000

    request to load text value to page

--- public/js/apps.js
+++ public/js/apps.js
@@ -1,1 +1,32 @@
-//
+var apiKey = 'nntbe78g2yfwq7wcwbzgpm2w';
+
+$( document ).ready( function () {
+
+  $( '#searchBtn' ).click( function () {
+    var textInput = $( '#input' ).val();
+
+    $.ajax('https://api.gettyimages.com/v3/search/images?phrase=' + textInput, {
+      headers : {
+        'Api-Key': apiKey,
+      },
+      method : 'GET'
+    }).done( function( data ) {
+      console.log(data);
+
+      var images = data.images;
+
+      images.forEach(function(img){
+
+        var imgUri = img.display_sizes[0].uri;
+
+        var contentBlock = $( '<img>', {
+          src : imgUri,
+          class : 'contentImg'
+        });
+
+        $( '.container' ).append( contentBlock );
+
+      }); // end forEach
+    }); // end AJAX
+  }); // end #searchBtn.click()
+}); // end .ready
