ca65871614edfc9f329b3fec90641addc81e0e9e
RizOrt218 <rizhiafalen@gmail.com>
2016-04-04 19:07:29 -1000

    wip style image content

--- public/js/apps.js
+++ public/js/apps.js
@@ -2,9 +2,11 @@ var apiKey = 'nntbe78g2yfwq7wcwbzgpm2w';
 
 $( document ).ready( function () {
 
+  // button search and populates image block with search text
   $( '#searchBtn' ).click( function () {
     var textInput = $( '#input' ).val();
 
+    // AJAX request to gettyimages api
     $.ajax('https://api.gettyimages.com/v3/search/images?phrase=' + textInput, {
       headers : {
         'Api-Key': apiKey,
@@ -15,16 +17,18 @@ $( document ).ready( function () {
 
       var images = data.images;
 
+      //append each images data
       images.forEach(function(img){
 
         var imgUri = img.display_sizes[0].uri;
 
+        //creating img script for each photo url
         var contentBlock = $( '<img>', {
           src : imgUri,
           class : 'contentImg'
         });
 
-        $( '.container' ).append( contentBlock );
+        $( '.images' ).append( contentBlock );
 
       }); // end forEach
     }); // end AJAX
